54079ec16ebda4cf1e1a05b14df327aa3cc91713
mmg114 <Mauricio Munoz Guzman>
2023-09-21 21:15:54 -0500

    Primer commit

---
+++ src/main/java/B1/Clase1.java
@@ -0,0 +1,14 @@
+package B1;
+
+public class Clase1 {
+
+    public static void main(String[] args) {
+        System.out.println(saludar("Mauricio"));
+    }
+
+
+    public static String saludar(String nombre){
+        return "Hola "+nombre;
+    }
+
+}
---
+++ src/main/java/B1/Clase2.java
@@ -0,0 +1,37 @@
+package B1;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Clase2 {
+    final BigDecimal NUMERO_GRANDE = new BigDecimal(0);
+
+    public static void main(String[] args) {
+        // declaracion, inicializacion y uso;
+
+        // Objetos o Wrapper
+        //Cadena de caracter
+        String valor1; // si esta de esta forma la variable es null
+        String valor2="Mauricio";//Variable inicializada
+
+        Date horaActual;//Declaracion
+        Date horaSiguiente = new Date();
+
+        //DATOS PRIMITIVOS
+        //numero
+        int num1=0;
+        long numLargo=1000;
+
+        //Valores Decimales
+        double num2=0.1;
+
+
+        //boleanos .... Logicos
+        boolean logico=true;
+        boolean logico2=false;
+
+        char val3='a';
+        short valorCorto;
+
+    }
+}
