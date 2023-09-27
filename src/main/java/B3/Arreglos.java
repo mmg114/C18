5bca5cea1978e819981b7759b53b9a98447ba02e
mmg114 <Mauricio Munoz Guzman>
2023-09-26 19:57:02 -0500

    For and fori

--- src/main/java/B3/Arreglos.java
+++ src/main/java/B3/Arreglos.java
@@ -1,6 +1,9 @@
 package B3;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arreglos {
 
     public static void main(String[] arg) {
@@ -9,9 +12,23 @@ public class Arreglos {
          nombres[0]="Mauricio";
          nombres[1]="Carlos";
          nombres[2]="Maria";
-         System.out.println(nombres[0]);
 
-        System.out.println(numeros[4]);
+
+
+
+         List<String> nombres2 = new ArrayList<>();
+
+         nombres2.add("Juan");
+         nombres2.add("camilo");
+         nombres2.add("pedro");
+
+        for (String nombre:nombres2) {
+            System.out.println(nombre);
+        }
+
+        for (int i = 0; i < nombres.length  ; i++) {
+            System.out.println(nombres[i]);
+        }
     }
 
 }
